e4a337595348d5cec87dec8d6a63c69017d7a85f
RitwikKapoor <kapoorritwik2020@gmail.com>
2024-02-05 23:13:00 +0530

    reading data from sector of hard disk using CHS

--- boot.asm
+++ boot.asm
@@ -1,4 +1,3 @@
-
 ORG 0	
 BITS 16
 
@@ -11,12 +10,6 @@ times 33 db 0
 start:
 	jmp 0x7c0:step2
 
-handle_zero:
-	mov ah, 0eh
-	mov al, 'A'
-	mov bx, 0x00
-	int 0x10
-	iret	 
 
 step2:
 	cli ; clear interrupt
@@ -27,17 +20,24 @@ step2:
 	mov ss, ax
 	mov sp, 0x7c00
 	sti	; enable interrupt
+	mov ah, 2
+	mov al, 1
+	mov ch, 0
+	mov cl, 2
+	mov dh, 0
+	mov bx, buffer
+	int 0x13
+	jc error
 
-	mov word[ss: 0x00], handle_zero
-	mov word[ss: 0x02], 0x7c0
-
-
-	mov ax, 0x00
-	div ax
-
-
-	mov si, message
+	mov si, buffer
 	call print
+	jmp $
+
+error:
+	mov si, error_message
+	call print
+	jmp $
+
 	jmp $
 
 print:
@@ -56,7 +56,30 @@ print_char:
 	int 0x10
 	ret
 
-message: db 'Hello World', 0
+error_message: db 'Failed to load sector', 0
 
 times 510-($ - $$) db 0 
 dw 0xAA55
+
+buffer:
+
+
+;DISK - READ SECTOR(S) INTO MEMORY
+;AH = 02h
+;AL = number of sectors to read (must be nonzero)
+;CH = low eight bits of cylinder number
+;CL = sector number 1-63 (bits 0-5)
+;high two bits of cylinder (bits 6-7, hard disk only)
+;DH = head number
+;DL = drive number (bit 7 set for hard disk)
+;ES:BX -> data buffer
+;
+;Return:
+;CF set on error
+;if AH = 11h (corrected ECC error), AL = burst length
+;CF clear if successful
+;AH = status (see #00234)
+;AL = number of sectors transferred (only valid if CF set for some
+;BIOSes)
+
+
